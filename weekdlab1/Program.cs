6adcd6655dbde83630d307385c33149b81b21609
cappow26 <52836248+cappow26@users.noreply.github.com>
2020-04-28 20:26:00 -0500

    Add files via upload

---
+++ weekdlab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekdlab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double curBal = 45.32;
+
+            double amount;
+            Console.Write("Please enter a amount to update account by $");
+            amount = Double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Customer's balance (before transaction) = $");
+            Console.WriteLine(curBal + "\n");
+            Console.Write("Requested update amount = $");
+            Console.WriteLine(amount + "\n");
+
+            double actAmount;
+            actAmount = transaction(curBal, amount);
+            curBal += actAmount;
+
+            Console.Write("Actual update amount = $");
+            Console.WriteLine(actAmount + "\n");
+            Console.Write("Customer's balance (after transaction) = $");
+            Console.WriteLine(curBal + "\n");
+
+            Console.WriteLine("\nThank you and good-bye!\n");
+            Console.Read();
+        }
+
+        public static double transaction(double bal, double amount)
+        {
+            if(bal + amount < 0){
+                return bal;
+            } else {
+                return amount;
+            }
+        }
+    }
+}
---
+++ weekdlab2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+         * Answers to questions
+         * 1. No because C# will convert it
+         * 2. Yes because of order of operations it will not be calculated correctly based of how averages work
+         * 3. Yes, average (double, double, double) will be used, it will convert all numbers do double since one of them is a double
+         */
+
+namespace weekdlab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 3, c = 5;
+            double x = 2.2, y = 4.4, z = 6.6, ans;
+
+            ans = average(a, b);
+            Console.WriteLine("\naverage(a, b) = " + ans);
+
+            ans = average(a, b, c);
+            Console.WriteLine("\naverage(a, b, c) = " + ans);
+
+            ans = average(x, y);
+            Console.WriteLine("\naverage(x, y) = " + ans);
+
+            ans = average(x, y, z);
+            Console.WriteLine("\naverage(x, y, z) = " + ans);
+
+            Console.Read();
+        }
+
+        public static double average(int n1, int n2)
+        {
+            return (n1 + n2) / 2.0;
+        }
+
+        public static double average(int n1, int n2, int n3)
+        {
+            return (n1 + n2 + n3) / 3.0;
+        }
+
+        public static double average(double n1, double n2)
+        {
+            return (n1 + n2) / 2.0;
+        }
+
+        public static double average(double n1, double n2, double n3)
+        {
+            return (n1 + n2 + n3) / 3.0;
+        }
+    }
+}
